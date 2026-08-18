bc630fbfb60a0a128ad5b9fabc868095130eb387
Jeffrey Miller <jeffrey.miller@gmail.com>
2026-08-18 12:04:46 -0700

    first 5 trees

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     conn = sqlite3.connect("/data/trees/trees.db", isolation_level=None)
-    return conn.execute('SELECT name FROM sqlite_schema WHERE type="table"').fetchall()
+    return conn.execute('SELECT * FROM trees LIMIT 5').fetchall()
 
 
 if __name__ == "__main__":
