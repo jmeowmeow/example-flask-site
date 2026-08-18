117edff39a95c924c1659ec3ecbc5bf99b5e3ec6
ndrooo <git@ndr.ooo>
2026-08-18 15:39:36 -0400

    Having some trouble accessing the ID...

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello_world():
     conn = sqlite3.connect("/data/trees/trees.db", isolation_level=None)
     data = conn.execute("SELECT OBJECTID FROM trees LIMIT 10").fetchall()
-    trees = [{id: row[0]} for row in data]
+    trees = [row for row in data]
     return render_template("index.html", trees=trees)
 
 
