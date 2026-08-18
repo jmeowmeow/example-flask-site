80798158f2016bb2d241e38d07aeb8b8905be2d8
ndrooo <git@ndr.ooo>
2026-08-18 15:31:30 -0400

    Add a simple template

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import sqlite3
 import re
+import sqlite3
 from datetime import datetime
 
 from flask import Flask, render_template
@@ -10,14 +10,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     conn = sqlite3.connect("/data/trees/trees.db", isolation_level=None)
-    return conn.execute('SELECT * FROM trees LIMIT 5').fetchall()
+    return conn.execute("SELECT * FROM trees LIMIT 5").fetchall()
+
 
 @app.route("/tree/<tree_id>")
 def get_tree(tree_id):
     conn = sqlite3.connect("/data/trees/trees.db", isolation_level=None)
     if re.search("^[0-9]+$", tree_id) is None:
-       return "Tree not found.", 400
-    return conn.execute(f'SELECT * FROM trees WHERE OBJECTID={tree_id}').fetchall()
+        return "Tree not found.", 400
+    tree = conn.execute(f"SELECT * FROM trees WHERE OBJECTID={tree_id}").fetchone()
+    return render_template("tree.html", species=tree[8], data=tree)
 
 
 if __name__ == "__main__":
