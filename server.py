43054918a60a7920cb9970bd197f4caec52d3934
Jeffrey Miller <jeffrey.miller@gmail.com>
2026-08-18 14:05:50 -0700

    try /toptrees by variety

--- server.py
+++ server.py
@@ -26,6 +26,17 @@ def get_tree(tree_id):
     tree = conn.execute(f"SELECT * FROM trees WHERE OBJECTID={tree_id}").fetchone()
     return render_template("tree.html", species=tree[8], data=tree)
 
+@app.route("/toptrees/<myvariety>")
+def get_top_trees(myvariety):
+    varieties = [ "oak", "maple", "apple", "linden", "plane", "elm", "hackberry" ]
+    conn = sqlite3.connect("/data/trees/trees.db", isolation_level=None)
+    if re.search("^[A-Za-z]+$", myvariety) is None:
+        myvariety = "elm"
+    data = conn.execute(
+        f"SELECT GenusSpecies as variety, COUNT(*) as pop FROM trees WHERE GenusSpecies LIKE '%{myvariety}%' GROUP BY variety ORDER BY pop DESC LIMIT 10 "
+    ).fetchall()
+    specieslist = [row[0] for row in data]
+    return render_template("toptrees.html", myvariety=myvariety, specieslist=specieslist, varieties=varieties)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
